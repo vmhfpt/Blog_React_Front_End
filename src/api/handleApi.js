107e2dc97d3a9a69b369bcff68e36755d6bb67af
vmhungltt <vuminhhungltt904@gmail.com>
2022-12-01 04:05:46 +0700

    change url api

--- src/api/handleApi.js
+++ src/api/handleApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import queryString from 'query-string';
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://blog.diaocconsole.tk',
   paramsSerializer: params => queryString.stringify(params),
   
 });
